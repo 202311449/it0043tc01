d9cef058ff65c8933e93d73839b9da81b2a1150b
202311449 <167260567+202311449@users.noreply.github.com>
2024-05-15 19:38:06 +0800

    Add files via upload

---
+++ Lindo/Lindo.cpp
@@ -0,0 +1,86 @@
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap')
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Lato;
+}
+
+.announcement {
+  background: url('maxresdefault.jpg') no-repeat center center;
+  background-size: cover;
+  padding: 100px;
+  box-sizing: border-box;
+  text-align: center;
+  color: black;
+  height: 150vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+
+
+.content {
+  background: white;
+  padding: 50px;
+  border-radius: 60px;
+  width: 70%;
+
+}
+
+.heading {
+  font-size: 60px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.subheading {
+  font-size: 30px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  color: grey;
+}
+
+p {
+  font-size: 20px;
+  margin-bottom: 15px;
+  line-height: 1.5;
+}
+
+.highlight {
+  color: #008000; 
+  font-weight: bold;
+}
+
+.conclusion {
+  font-size: 1.3em;
+  font-weight: bold;
+  color: #006400; 
+  margin-top: 20px;
+}
+
+.text {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  color: darkgreen;
+}
+
+.lower_text{
+  background: darkorange;
+  padding: 20px 10px;
+  border-radius: 5px;
+  color: white;
+
+}
+
+
+.hashtag {
+  background: green;
+  padding: 20px 10px;
+  border-radius: 5px;
+  color: white;
+}
